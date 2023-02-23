2944a072bcfe7f9205ae9ce95534d5b3a4995f30
PHCaesar <philipp.cserich@gmail.com>
2023-02-23 12:24:01 +0100

    Theme fix

--- exec.bash
+++ exec.bash
@@ -1,3 +1,3 @@
 project_root=$(pwd)
 
-docker run --rm --name=diplomathesis -it -v $project_root/Documentation:/documents -v $project_root/Assets:/Assets asciidoctor/docker-asciidoctor asciidoctor-pdf --theme $project_root/Assets/theme.yml -a pdf-fontsdir=$project_root/Assets/Fonts index.adoc
+docker run --rm --name=diplomathesis --workdir="" -it -v $project_root/Documentation:/Documentation -v $project_root/Assets:/Assets asciidoctor/docker-asciidoctor asciidoctor-pdf --theme /Assets/theme.yml -a pdf-fontsdir=/Assets/Fonts /Documentation/index.adoc
--- exec.bat
+++ exec.bat
@@ -1,3 +1,1 @@
-docker run --rm --name=diplomathesis -it -v %cd%/Documentation:/Documentation -v %cd%/Assets:/Assets asciidoctor/docker-asciidoctor asciidoctor-pdf --theme %cd%/Assets/theme.yml -a pdf-fontsdir=%cd%/Assets/Fonts documents/index.adoc
-
-waitForString(" ")
+docker run --rm --name=diplomathesis --workdir="" -it -v %cd%/Documentation:/Documentation -v %cd%/Assets:/Assets asciidoctor/docker-asciidoctor asciidoctor-pdf --theme /Assets/theme.yml -a pdf-fontsdir=/Assets/Fonts /Documentation/index.adoc
