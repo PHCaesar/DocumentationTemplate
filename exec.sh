72bf3e376cdab541c93db0cdef19284e03241715
PHCaesar <philipp.cserich@gmail.com>
2023-03-15 12:31:12 +0100

    sh files

--- exec.sh
+++ exec.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 project_root=$(pwd)
 
 docker run --rm --name=diplomathesis --workdir="" -it -v $project_root/Documentation:/Documentation -v $project_root/Assets:/Assets asciidoctor/docker-asciidoctor asciidoctor-pdf --theme /Assets/theme.yml -a pdf-fontsdir=/Assets/Fonts /Documentation/index.adoc
--- pro-exec.sh
+++ pro-exec.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 project_root=$(pwd)
 
 docker run --rm --name=diplomathesis -it -v $project_root/Documentation:/documents -v $project_root/Assets:/Assets asciidoctor/docker-asciidoctor
